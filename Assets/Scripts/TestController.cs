c8e8c95fbb1fc1ef628c607393568724036391c3
Matthew Scaperoth <scaperoth@gmail.com>
2017-12-06 13:24:41 -0600

    first commit

---
+++ Assets/Scripts/TestController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TestController : MonoBehaviour {
+
+	private Text _textBox;
+
+	private string _defaultText = "Available Devices: ";
+
+	// Use this for initialization
+	void Start () {
+		_textBox = FindObjectOfType<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		_textBox.text = _defaultText+"\n";
+		for (int cameraIndex = 0; cameraIndex < WebCamTexture.devices.Length; cameraIndex++) {
+			
+			_textBox.text += WebCamTexture.devices[cameraIndex].name + "\n";
+		}
+	}
+}
